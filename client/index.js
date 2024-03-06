ab418c9eb95b0b09bfe83141fb0d43f1e53eb561
ManoranjanJena24 <mjena@mitaoe.ac.in>
2024-03-06 23:53:58 +0530

    Created Signup Screen Frontend

---
+++ client/index.js
@@ -0,0 +1,22 @@
+let url ="http://localhost:3000"
+
+function handleSubmitForm(event) {
+    event.preventDefault();
+    const signUpData = {
+        name: event.target.name.value,
+        email: document.getElementById('email').value,
+        password: event.target.password.value,
+    }
+    event.target.reset();
+    postSignUpData(signUpData)
+
+}
+
+
+function postSignUpData(data) {
+    axios.post(`${url}/user/signup`, data).then(() => {
+        
+    }).catch((err) => {
+        console.log(err)
+    })
+}
