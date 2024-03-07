4330b384fdccafd41357e7b32b63253a7bb423ff
ManoranjanJena24 <mjena@mitaoe.ac.in>
2024-03-07 16:18:35 +0530

    Edited CSS

--- client/index.js
+++ client/index.js
@@ -1,6 +1,27 @@
-let url ="http://localhost:3000"
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+const sign_in_btn2 = document.querySelector("#sign-in-btn2");
+const sign_up_btn2 = document.querySelector("#sign-up-btn2");
+sign_up_btn.addEventListener("click", () => {
+    container.classList.add("sign-up-mode");
+});
+sign_in_btn.addEventListener("click", () => {
+    container.classList.remove("sign-up-mode");
+});
+sign_up_btn2.addEventListener("click", () => {
+    container.classList.add("sign-up-mode2");
+});
+sign_in_btn2.addEventListener("click", () => {
+    container.classList.remove("sign-up-mode2");
+});
 
-function handleSubmitForm(event) {
+
+
+
+let url = "http://localhost:3000"
+
+function handleSignUpForm(event) {
     event.preventDefault();
     const signUpData = {
         name: event.target.name.value,
@@ -15,11 +36,11 @@ function handleSubmitForm(event) {
 
 function postSignUpData(data) {
     axios.post(`${url}/user/signup`, data).then(() => {
-        
+
     }).catch((err) => {
         console.log(err)
         const errormsg = document.getElementById('errormsg')
         errormsg.innerHTML = err.message
-       
+
     })
 }
