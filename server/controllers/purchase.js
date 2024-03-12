93a0c29ca1a522758ea25d7f7da1a6ce0c83ed04
ManoranjanJena24 <mjena@mitaoe.ac.in>
2024-03-12 14:14:36 +0530

    Added ispremiumuser column to user table in database

--- server/controllers/purchase.js
+++ server/controllers/purchase.js
@@ -2,7 +2,7 @@ const Razorpay = require('razorpay')
 const Order = require('../models/order')
 
 const purchasePremium = async (req, res, next) => {
-    console.log(process.env.RAZORPAY_KEY_ID,' key ')
+    console.log(process.env.RAZORPAY_KEY_ID, ' key ')
     try {
         var rzp = new Razorpay({
             key_id: process.env.RAZORPAY_KEY_ID,
@@ -10,44 +10,56 @@ const purchasePremium = async (req, res, next) => {
         })
         const amount = 2500
         rzp.orders.create({
-            amount,currency:"INR"
-        }, (err,order) => {
+            amount, currency: "INR"
+        }, (err, order) => {
             if (err) {
                 throw new Error(JSON.stringify(err))
             }
             req.user.createOrder({
                 orderid: order.id,
-                status:'PENDING'
+                status: 'PENDING'
             }).then(() => {
                 return res.status(201).json({
                     order,
-                    key_id:rzp.key_id
-              })  
+                    key_id: rzp.key_id
+                })
             }).catch((err) => {
                 console.log(err)
             })
         })
     }
-    catch (err) { 
+    catch (err) {
         console.log(err)
         rest.status(403).json({
             message: "Something Went Wrong",
-            error:err
+            error: err
         })
     }
 }
 
 
 const updateTransactionStatus = async (req, res, next) => {
-    // try {
-    //     const {
-    //         payment_id,
-    //         order_id
-    //     } = req.body
-    //     Order.findOne({
-    //         where:
-    //     })
-    // }
+    try {
+        const {
+            payment_id,
+            order_id
+        } = req.body
+        Order.findOne({
+            where: {
+                orderid: order_id
+            }
+        }).then((order) => {
+            order.update({ paymentid: payment_id, status: 'SUCCESSFUL' }).then(() => {
+                req.user.update({ ispremiumuser: true }).then(() => {
+                    return res.status(202).json({ success: true, message: 'TRANSCTION SUCCESSFULL' })
+                }).catch(err => console.log(err))
+            }).catch(err => console.log(err))
+        })
+
+    }
+    catch (err) {
+        console.log(err)
+    }
 }
 
 
