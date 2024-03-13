9d6ef1a9701122466b3a36ab2eb170531548ae80
ManoranjanJena24 <mjena@mitaoe.ac.in>
2024-03-13 20:18:25 +0530

    Used Transaction to add Expense and update total expenses for the respective users

--- server/controllers/expense.js
+++ server/controllers/expense.js
@@ -4,101 +4,98 @@ const sequelize = require('../utils/database')
 
 
 
-// exports.postAddExpense = async (req, res, next) => {
+// exports.postAddExpense = (req, res, next) => {
 //     const amount = req.body.amount;
 //     const description = req.body.description;
 //     const category = req.body.category;
-//     const userId = req.user.id;
+//     //  console.log(req.user)
+//     const userId = req.user.id//changes
 //     const expenseAmount = +amount;
+//     console.log(typeof (expenseAmount))
+//     const totalExpense = req.user.totalexpense + expenseAmount;
 
-//     console.log('user id inside add expense', req.user);
+//     console.log('user id inside add expense', req.user)
 
-//     try {
-//         console.log("Inside Add User");
-//         const t = await sequelize.transaction();
+//     console.log("Inside Add User")
+//     req.user.createExpense({
+//         amount: amount,
+//         description: description,
+//         category: category,
+//     })
+//         // .then(result => {
+//         //     console.log("Created Expense");
 
-//         // Create expense
-//         const result = await req.user.createExpense({
-//             amount: amount,
-//             description: description,
-//             category: category,
-//         }, { transaction: t });
-
-//         console.log("Created Expense");
-
-//         // Find the user by primary key
-//         const user = await User.findByPk(req.user.id, { transaction: t });
-//         if (!user) {
-//             await t.rollback();
-//             return res.status(404).json({ error: 'User not found' });
-//         }
-
-//         // Update totalExpense in the user table
-//         const totalExpense = req.user.totalexpense + expenseAmount;
-//         await user.update({ totalexpense: totalExpense }, { transaction: t });
-
-//         await t.commit();
-//         console.log("Updated totalExpense in user table");
-//         res.json(result);
-//     } catch (err) {
-//         console.log(err);
-//         res.status(500).json({ error: 'Internal Server Error' });
-//     }
+//         //     res.json(result);
+//         // })
+//         .then(result => {
+//             console.log("Created Expense");
+//             // Find the user by primary key
+//             User.findByPk(req.user.id)
+//                 .then(user => {
+//                     if (!user) {
+//                         return res.status(404).json({ error: 'User not found' });
+//                     }
+//                     // Update totalExpense in the user table
+//                     user.update({ totalexpense: totalExpense })
+//                         .then(() => {
+//                             console.log("Updated totalExpense in user table");
+//                             res.json(result);
+//                         })
+//                         .catch(err => {
+//                             console.log(err);
+//                             res.status(500).json({ error: 'Internal Server Error' });
+//                         });
+//                 })
+//                 .catch(err => {
+//                     console.log(err);
+//                     res.status(500).json({ error: 'Internal Server Error' });
+//                 });
+//         })
+//         .catch(err => {
+//             console.log(err);
+//             res.status(500).json({ error: 'Internal Server Error' });
+//         });
 // };
 
-exports.postAddExpense = (req, res, next) => {
+
+exports.postAddExpense = async (req, res, next) => {
     const amount = req.body.amount;
     const description = req.body.description;
     const category = req.body.category;
-    //  console.log(req.user)
-    const userId = req.user.id//changes
+    const userId = req.user.id;
     const expenseAmount = +amount;
-    console.log(typeof (expenseAmount))
-    const totalExpense = req.user.totalexpense + expenseAmount;
 
-    console.log('user id inside add expense', req.user)
+    try {
+        // Start a transaction
+        const transaction = await sequelize.transaction();
 
-    console.log("Inside Add User")
-    req.user.createExpense({
-        amount: amount,
-        description: description,
-        category: category,
-    })
-        // .then(result => {
-        //     console.log("Created Expense");
+        // Create the expense
+        const createdExpense = await Expense.create({
+            amount: amount,
+            description: description,
+            category: category,
+            userId: userId // Assuming userId is a foreign key in Expense model
+        }, { transaction });
 
-        //     res.json(result);
-        // })
-        .then(result => {
-            console.log("Created Expense");
-            // Find the user by primary key
-            User.findByPk(req.user.id)
-                .then(user => {
-                    if (!user) {
-                        return res.status(404).json({ error: 'User not found' });
-                    }
-                    // Update totalExpense in the user table
-                    user.update({ totalexpense: totalExpense })
-                        .then(() => {
-                            console.log("Updated totalExpense in user table");
-                            res.json(result);
-                        })
-                        .catch(err => {
-                            console.log(err);
-                            res.status(500).json({ error: 'Internal Server Error' });
-                        });
-                })
-                .catch(err => {
-                    console.log(err);
-                    res.status(500).json({ error: 'Internal Server Error' });
-                });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({ error: 'Internal Server Error' });
-        });
+        // Update user's totalExpense
+        const user = await User.findByPk(userId, { transaction });
+        if (!user) {
+            await transaction.rollback();
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        const totalExpense = user.totalexpense + expenseAmount;
+        await user.update({ totalexpense: totalExpense }, { transaction });
+
+        // Commit the transaction
+        await transaction.commit();
+
+        res.json(createdExpense);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 };
-
 exports.getAllExpenses = async (req, res, next) => {
     User.findAll({
         attributes: ['id', 'name', 'totalexpense'], order: [['totalexpense', 'DESC']]
